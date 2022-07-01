b999987076900f0feeeb56383ff6ce01a8338e9d
maupat552 <mercyaupat7@gmail.com>
2022-07-01 05:11:26 -0700

    Task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0
-*/
-
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	int p, q;
+	int c, i, k, j;
 
-	for (p = 0; p <= 98; p++)
+	for (c = 48; c <= 57; c++)
 	{
-		for (q = p + 1; q <= 99; q++)
+		for (i = 48; i <= 57; i++)
 		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
 
-			if (p == 98 && q == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
 		}
 	}
 
