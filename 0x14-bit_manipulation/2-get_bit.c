4f8438da6996a1fb92cd490d33e40237067cd239
maupat552 <mercyaupat7@gmail.com>
2022-08-18 02:41:58 -0700

    Task 0 to 5

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - gets a bit, 1 or 0, at index
+ * @n: number to get bit from
+ * @index: index to find bit at
+ *
+ * Return: 1 or 0 depending if bit is lit or not
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int ret = 0;
+	unsigned long int mask = 0x01;
+
+	mask = mask << index;
+	if (mask == 0)
+		return (-1);
+	ret = ((n & mask)) ? 1 : 0;
+	return (ret);
+}
