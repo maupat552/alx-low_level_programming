c57923613a99d19106f0ed2a6da62de2ebada62c
Mercy Aupat <89969949+maupat552@users.noreply.github.com>
2022-08-22 14:59:57 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,50 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
- * @filename: path of file
- * @text_content: content to be write on new file or truncated on existing
- * Return: 1 on Success, -1 on failure
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
  */
-int create_file(const char *filename, char *text_content)
+ssize_t _strlen(const char *str)
 {
-	int count, len = 0, fd;
+	ssize_t len = 0;
 
-	if (filename == NULL)
+	if (!str)
 		return (-1);
 
-	/* Open file and get file descriptor */
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd == -1)
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * append_text_to_file - append text to the end of a file
+ * @filename: the name of the file to append to
+ * @text_content: the data to append to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t b_written = 0;
+	int fd;
+
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-	}
-	/* write into file description */
-	count = write(fd, text_content, len);
-	if (count == -1)
-	{
-		close(fd);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd < 0)
 		return (-1);
-	}
+
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
+
 	close(fd);
-	return (1);
 
+	if (b_written < 0)
+		return (-1);
+	return (1);
+}nclude "main.h"
