6e2c0c2caff8f9a935c1f3b97f85ed08c5fa147a
Mercy Aupat <89969949+maupat552@users.noreply.github.com>
2022-08-22 14:32:45 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,8 +6,9 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * Otherwise - 1.
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
